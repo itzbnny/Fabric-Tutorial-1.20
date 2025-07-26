08de26176819b164a48ed36227f24b944f062cfb
itzbnny <ian.bennett0314@gmail.com>
2025-07-26 10:41:08 -0400

    fix: new repo

---
+++ src/main/java/net/itzbnny/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.itzbnny.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.itzbnny.tutorialmod.block.ModBlocks;
+import net.itzbnny.tutorialmod.item.ModItemGroups;
+import net.itzbnny.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
